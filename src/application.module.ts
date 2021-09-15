77bda6fa157a07536863942ccffcbc0212c07c13
배건태 <kenta.03.1110@gmail.com>
2021-09-15 17:57:59 +0900

    :fire: Remove blog related code from application.module.ts
    application.module.ts 에서 원래 프로젝트에 작성되어 있던 blog 와 관련된 코드 삭제.

--- src/application.module.ts
+++ src/application.module.ts
@@ -1,20 +1,18 @@
-import { Module } from '@nestjs/common';
-import { RenderModule } from 'nest-next';
-import Next from 'next';
-import { AppController } from './app.controller';
-import { BlogController } from './blog/blog.controller';
-import { BlogService } from './blog/blog.service';
+import { Module } from "@nestjs/common";
+import { RenderModule } from "nest-next";
+import Next from "next";
+import { AppController } from "./app.controller";
 
 @Module({
   imports: [
     RenderModule.forRootAsync(
       Next({
-        dev: process.env.NODE_ENV !== 'production',
+        dev: process.env.NODE_ENV !== "production",
         conf: { useFilesystemPublicRoutes: false },
-      }),
+      })
     ),
   ],
-  controllers: [AppController, BlogController],
-  providers: [BlogService],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
